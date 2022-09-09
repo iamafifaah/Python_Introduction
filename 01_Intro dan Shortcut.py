4f9f4e3aee19d39ecf034c2fdd138d621aabf03d
Nur Afifah <86917785+iamafifaah@users.noreply.github.com>
2022-09-09 13:06:24 +0700

    Add files via upload

---
+++ 01_Intro dan Shortcut.py
@@ -0,0 +1,36 @@
+print("Hello world")
+print("Okay")
+
+### memindahkan baris di python alt + tanda panah bawah atau atas
+### menghapus terminal dengan cls
+### python dijalankan line by line dan berurutan
+### baris kosong tidak akan dieksekusi
+### operator assignment tidak bisa diprint / ditampilkan
+### memilih baris dengan ctrl + l
+### membuat autocomplate dengan tab
+### mengcompile sebuah file python ke dalam bahasa c untuk
+    # memudahkan proses running dengan code 
+    # mengubah directory dengan code cd di terminal dan mengetik python main.asdfghjkl.py : sesuai nama di dalam folder compile, tab untuk autocomplate
+### python bahasa yang interpreted jadi butuh waktu lebih lama untuk dirunning
+"""
+Ctrl + Shift + P: menampilkan Command Palette
+Ctrl + P: membuka file.
+Ctrl + Shift + N: membuat Window baru. 
+Ctrl + Shift + W: menutup Window.
+Ctrl + X; memotong line
+Ctrl + C: copy line. 
+Alt + ↑ / ↓: berpindah ke line atas atau bawah. 
+Shift + Alt  + ↓ / ↑: copy line atas atau bawah. 
+Ctrl + Shift + K: menghapus line
+Ctrl + Home: menuju awal file.
+Ctrl + End: menuju file paling akhir. 
+Ctrl + ↑ / ↓: scroll line ke atas atau ke bawah. 
+Alt + PgUp / PgDn: scroll halaman ke atas atau ke bawah. 
+Ctrl + N: membuat file baru. 
+Ctrl + O: membuka file baru.
+Ctrl + S: simpan. 
+Ctrl + F4: tutup aplikasi
+Ctrl + d: untuk memilih satu huruf / kata dan bagian setelahnya dalam satu line
+"""
+a = 10
+print(a)
